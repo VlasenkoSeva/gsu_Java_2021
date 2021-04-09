22a8b853429ad3bd7682a09ab2ad83e11910f3bf
VlasenkoSeva <rx.kapusta@mail.ru>
2021-04-09 16:50:55 +0300

    Lesson5

--- Lesson5/src/FourthWork/Calculator.java
+++ Lesson5/src/FourthWork/Calculator.java
@@ -5,10 +5,13 @@ public class Calculator<T, sum> {
     public static void main(String[] args) {
         int T[] = {3, 7, 8, 6, 16, 5};
         int sum = 0;
+        int otric = 0;
         for (int i = 0; i < T.length; i++) {
             sum += T[i];
+            otric -= T[i];
         }
         System.out.println(sum);
+        System.out.println(otric);
     }
 }
 
